a39f5ee37fa4245ec9bde2a80a2f870f65bce4da
Romeo13card <romeo_13card_fan@outlook.com>
2025-11-24 18:04:30 +0300

    switch to new kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Clone kernel tree
-git clone --depth=1 -b r2 https://github.com/alternoegraha/kernel_xiaomi_sm6225.git kernel/xiaomi/fog
+git clone --depth=1 -b ye https://github.com/alternoegraha/kernel_xiaomi_sm6225  kernel/xiaomi/fog
